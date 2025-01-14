7dd58bcc885244fbc2ec1a070e483af819e4fa5a
Sagar Sinha <sagarsinha7777@gmail.com>
2025-01-14 17:10:24 +0530

    First Commit

--- bin/bookReviewsMergedApi.ts
+++ bin/bookReviewsMergedApi.ts
@@ -9,7 +9,7 @@ import { SchemaBreakingChangeDetectionStack } from '../lib/breakingChangeDetecti
 const app = new cdk.App();
 
 const defaultRegion = 'us-east-1'
-export const YOUR_REPOSITORY_NAME = "sagarsinha7777/aws-appsync-merged-api-ci-cd-sample";
+export const YOUR_REPOSITORY_NAME = "sagarsinha7777/aws-appsync-merged-api-typescript";
 new SchemaBreakingChangeDetectionStack(app, 'BreakingChangeDetection', {
     env: {
         region: defaultRegion
